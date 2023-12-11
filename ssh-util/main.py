555be204a1848a91a58c2e71158ebf297089afa9
cat <feemun@outlook.com>
2023-12-11 21:35:38 +0800

    v0.1 实现扫描网段下所有设备，并用shh连接

--- ssh-util/main.py
+++ ssh-util/main.py
@@ -13,24 +13,11 @@ def get_all_ips_in_subnet(subnet):
         return f"无效的子网掩码: {e}"
 
 
-def is_device_reachable(ip, port=22):
-    try:
-        # 尝试建立 socket 连接
-        with socket.create_connection((ip, port), timeout=3):
-            return True
-    except (socket.timeout, socket.error):
-        print(ip + "设备无法访问")
-        return False
-
-
 def check_devices_availability(ip_list):
-    available_devices = []
-
     for ip in ip_list:
-        if is_device_reachable(ip):
-            available_devices.append(ip)
-
-    return available_devices
+        if is_ssh_available(ip):
+            device_info = get_device_info(ip, 22, "catfish", "catfish")
+            print(device_info)
 
 
 def is_ssh_available(ip, port=22, username="catfish", password="catfish"):
@@ -38,14 +25,14 @@ def is_ssh_available(ip, port=22, username="catfish", password="catfish"):
         # 创建 SSH 客户端
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
         # 尝试连接
         client.connect(ip, port=port, username=username, password=password, timeout=1)
-
         # 关闭连接
         client.close()
+        print("连接成功" + ip)
         return True
     except (paramiko.AuthenticationException, paramiko.SSHException, socket.timeout):
+        print("连接失败" + ip)
         return False
 
 
@@ -63,10 +50,8 @@ def get_device_info(ip, port=22, username="catfish", password="catfish"):
         # 创建 SSH 客户端
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
         # 尝试连接
-        client.connect(ip, port, username, password, timeout=3)
-
+        client.connect(ip, port=port, username=username, password=password, timeout=1)
         # 获取主机名
         stdin, stdout, stderr = client.exec_command("hostname")
         device_info["Hostname"] = stdout.read().decode().strip()
@@ -83,24 +68,11 @@ def get_device_info(ip, port=22, username="catfish", password="catfish"):
     return device_info
 
 
-def check_ssh_availability(ip_list, port, username, password):
-    ssh_available_devices = []
-
-    for ip in ip_list:
-        if is_ssh_available(ip, port, username, password):
-            ssh_available_devices.append(ip)
-
-    return ssh_available_devices
-
-
-# 对所有设备发送ping
-# 如果3秒没回复，则判断为设备不存在或设备禁止ping
-# 如果设备回复了，则判断ssh端口是否提供服务
-# 如果设备ssh端口提供服务，则使用用户名和密码登录
+# 获取网段下所有ip
+# 遍历所有设备
+# 输入ip，端口，用户名，密码船舰ssh连接
+# 如果ssh连接成功，则返回主机信息
 
 if __name__ == '__main__':
-    # subnet = get_all_ips_in_subnet("192.168.0.100")
-    # available_devices = check_devices_availability(subnet)
-    # ssh_availability = check_ssh_availability(available_devices, 22, "catfish", "catfish")
-    info = get_device_info("192.168.0.100", 22, "catfish", "catfish")
-    print(info)
+    subnet = get_all_ips_in_subnet("192.168.31.1/24")
+    check_devices_availability(subnet)
